71852987b8dccb1077baa3f7f8e913c374e467b2
nabil <nabil.uiu.bd@gmail.com>
2025-03-04 16:02:59 +0600

    done data binding

--- hello-angular/src/app/app.component.ts
+++ hello-angular/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  title = 'Angular Blog App';
+  blogs = ['Blog 1', 'Blog 2', 'Blog 3'];
+  number = 0;
+
+  onClick(event: Event) {
+    event.stopPropagation();
+    console.log('Button Clicked', event);
+    this.number++;
+  }
+
+  onDivClicked() {
+    console.log('Div clicked');
+  }
+
+  div2Clicked() {
+    console.log('Div 2 clicked');
+  }
 }
+
+
--- hello-angular/src/app/app.module.ts
+++ hello-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostComponent
   ],
   imports: [
     BrowserModule,
